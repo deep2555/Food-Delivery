6f6167b641edb68ad394c8be7b4aee955600b155
deep2555 <deepanshuprashar87@gmail.com>
2025-07-07 20:26:36 +0200

    Initial setup

---
+++ fooddelapi/src/main/java/com/deepanshu/fooddelapi/FooddelapiApplication.java
@@ -0,0 +1,14 @@
+package com.deepanshu.fooddelapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FooddelapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FooddelapiApplication.class, args);
+		System.out.println("hi this is my first spring boot app");
+	}
+
+}
