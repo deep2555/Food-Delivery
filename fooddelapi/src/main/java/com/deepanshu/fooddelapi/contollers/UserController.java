a5e58caaf8fedf38733a5f42f283369a9e94370c
deep2555 <deepanshuprashar87@gmail.com>
2025-07-12 17:54:32 +0200

    done with some error

--- fooddelapi/src/main/java/com/deepanshu/fooddelapi/contollers/UserController.java
+++ fooddelapi/src/main/java/com/deepanshu/fooddelapi/contollers/UserController.java
@@ -1,8 +1,29 @@
 package com.deepanshu.fooddelapi.contollers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.deepanshu.fooddelapi.model.UsersDTO;
 
 @RestController
 public class UserController {
 
+	
+
+	@Autowired
+	private UserServiceImpl userServiceImpl;
+	
+	@PostMapping("/createUser")
+	public ResponseEntity<UsersDTO> createUser(@RequestBody UsersDTO usersDTO){
+		
+		System.out.println("inside the add Restaurant method ");
+		System.out.println("fething the restaurant details: "+ usersDTO.toString());
+		UsersDTO userResult =   userServiceImpl.createUser(usersDTO);		
+		return new ResponseEntity<UsersDTO>(userResult, HttpStatus.CREATED);
+		
+	}
 }
