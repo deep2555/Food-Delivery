e183116d0bcd8b7bf0986d54a4f11f91309876fd
deep2555 <deepanshuprashar87@gmail.com>
2025-07-14 21:46:57 +0200

    ok

--- fooddelapi/src/main/java/com/deepanshu/fooddelapi/services/UserServiceImpl.java
+++ fooddelapi/src/main/java/com/deepanshu/fooddelapi/services/UserServiceImpl.java
@@ -1,12 +1,20 @@
 package com.deepanshu.fooddelapi.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.deepanshu.fooddelapi.model.UsersDTO;
+import com.deepanshu.fooddelapi.repo.UserRepo;
 
-public class UserServiceImpl {
+public class UserServiceImpl implements UserService{
 
+	@Autowired
+	private UserRepo userRepo;
+	
+	@Override
 	public UsersDTO createUser(UsersDTO usersDTO) {
-		//add some tech
-		return null;
+		System.out.println("here inside creta user service "+ usersDTO.toString());	
+		// check this
+		return userRepo.save(usersDTO);
 	}
 
 }
