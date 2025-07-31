a4c11d18493feb486356d77167d724f918d17b94
Deepanshu Prashar <82754366+deep2555@users.noreply.github.com>
2025-07-31 21:09:29 +0200

    Update PageController.java

--- fooddelapi/src/main/java/com/deepanshu/fooddelapi/contollers/PageController.java
+++ fooddelapi/src/main/java/com/deepanshu/fooddelapi/contollers/PageController.java
@@ -1,5 +1,7 @@
 package com.deepanshu.fooddelapi.contollers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,40 +9,41 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PageController {
+	public static final Logger customLogger = LoggerFactory.getLogger(PageController.class);
 
 	
 
 	  @RequestMapping("/home")
 	    public String homePage() {
-		  System.out.println("inside the homPage method ");
+		  customLogger.debug("inside the home page method");
 	        return "index"; // loads templates/index.html
 	    }
 	  @RequestMapping("/register")
 	  public String registrationPage() {
-		  System.out.println("inside register page menthod");
+		  customLogger.debug("inside register page menthod");
 		  return "registration";
 	  }
 	  @RequestMapping("/login")
 	  public String loginPage() {
-		  System.out.println("inside Login page menthod");
+		  customLogger.debug("inside Login page menthod");
 		  return "login";
 	  }
 	  
 	  @RequestMapping("/dashboard")
 	  public String dashboard() {
-		  System.out.println("inside dashboard page menthod");
+		  customLogger.debug("inside dashboard page menthod");
 		  return "dashboard";
 	  }
 	  
 	  @RequestMapping("/restaurants")
 	  public String restaurant() {
-		  System.out.println("inside dashboard page menthod");
+		  customLogger.debug("inside dashboard page menthod");
 		  return "restaurant";
 	  }
 	  
 	  @RequestMapping("/restaurantsDetails")
 	  public String restaurantDetail() {
-		  System.out.println("inside dashboard page menthod");
+		  customLogger.debug("inside dashboard page menthod");
 		  return "restaurantDetail";
 	  }
 }
